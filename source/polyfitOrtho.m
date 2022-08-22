ed38831ed1f384b56490286e4f8794c1bf23ced2
Jason H Nicholson <1058191+jasonnicholson@users.noreply.github.com>
2022-08-21 22:42:01 -0500

    Working on updating documentation

--- source/polyfitOrtho.m
+++ source/polyfitOrtho.m
@@ -1,14 +1,66 @@
-function [resultsTable] = polyfitOrtho(x,f,k)
+function [resultsTable] = polyfitOrtho(x_mu,f_mu,k)
     %polyfitOrtho fits polynomial orthogonal
-    %   resultsTable = polyfitOrtho(x,f,k)
+    %   resultsTable = polyfitOrtho(x_mu,f_mu,k)
     %
     %% Inputs
-    % x - vector of x points for fit.
-    % f - vector of y points for fit. Must be the same size as x.
+    % x_mu - vector of x_mu points for fit. m = numel(x_mu) by definition.
+    % f_mu - vector of y points for fit. Must be the same size as x_mu.
+    % k - polynomial fit order. This algorithm requires k<=m-1. 
     %
     %% Outputs
-    % resultsTable - 
+    % resultsTable - A table containing the coefficients, alphas, betas, and variances'. The table always has k+1 rows.
+    %     s - is the cofficients of each polynomial.
+    %     alpha - The 1st coefficient it the orthogonality procedure.
+    %     beta - The 2nd coefficient in the orthogonality procedure.
+    %     variance - The variance of the residuals for each polynomial fit. 
+    %
     %% Description
+    % This functions fits a polynomial of the form.
+    %
+    %   y_k(x) = s_0*p_0(x) + s_1*p_1(x) + ... + s_k*p_k(x)
+    %
+    %   where 
+    %
+    %   - p_i(x) is polynomial of order i. By definition, p_i is orthogonal to p_j as long as i~=j on the inner product,
+    %   sum(p_j(x_mu).*p_i(x_mu)). i in p_i(x) is the order of the polynomial. 
+    %   - s_i is the coefficient of p_i.
+    %   - y_k(x) is then a polynomial of order k constructed from orthogonal polynomials.
+    %
+    %   The orthogonal polynomials, p_i, are generated using the following definition:
+    %
+    %   p_0(x) = 1;
+    %   p_1(x) = x*p_0(x) - alpha_1*p_0(x);
+    %   p_2(x) = x*p_1(x) - alpha_2*p_1(x) - beta_1*p_0(x);
+    %   ...      ...
+    %   p_(i+1)(x) = x*p_i(x) - alpha_(i+1)*p_i(x) - beta_i*p_(i-1)(x);
+    %
+    %   Alpha and Beta are chosen to make the orthogonality relations hold. See Forsythe, [1] for derivation. The
+    %   formulas for alpha's and beta's are
+    %  
+    %   alpha_(i+1) = sum(x_mu.*p_i(x_mu).^2)./sum(p_i(x_mu).^2); 
+    %   beta(i) = sum(x_mu.*p_i(x_mu).*p_(i-1)(x_mu))./sum(p_(i-1)(x_mu).^2);
+    %   
+    %   % Note that more involved, less intuitive formula are used in the code. These formulas can be found in Forsythe,
+    %   [1].
+    %
+    %  The beauty of this formulation is that the least squares problem is diagonal:
+    %
+    %  w_ii = sum(p_i(x_mu).*p_i(x_mu));
+    %  omega_i = sum(f_mu.*p_i(x_mu);
+    %  
+    %  w_00*s_0                  = omega_0
+    %         w11*s_1            = omega_1
+    %                ...
+    %                   w_kk*s_k = omega_k 
+    %
+    %  
+    %   Other important properties:
+    %   - The algorithm does not require forming the A matrix in the least squares problem Ax=b. 
+    %   - The algorithm finds each coefficient s_i, in order form 0 to k. This has the property that you get all
+    %   polynomial fits from 0 to k by requesting kth polynomial fit.
+    %   - The variance of the residuals is calculated for each i from 0 to k by an update formula.
+    %   - The least square problem is better formed and therefore higher order of polynomials can fitted then with
+    %   Vandermonde matrices (i.e. x.^(0:k), aka monomial basis).
     %
     %% Example
     % Fit a 100th degree polynomial to the Runge function on 150 Chebyshev points. Compare to polyfit/Vandermonde
@@ -24,7 +76,7 @@ function [resultsTable] = polyfitOrtho(x,f,k)
     %
     %% References
     % The algorithm and nomenclature comes from Forsythe 1957.
-    % Forsythe, George E. "Generation and use of orthogonal polynomials for data-fitting with a digital computer."
+    % [1] Forsythe, George E. "Generation and use of orthogonal polynomials for data-fitting with a digital computer."
     % Journal of the Society for Industrial and Applied Mathematics 5.2 (1957): 74-88.
     %
     % See also polyvalOrtho, polyfit, polyval.
@@ -32,15 +84,15 @@ function [resultsTable] = polyfitOrtho(x,f,k)
     
     %% Input Checking
     arguments
-        x (:,1) double;
-        f (:,1) double;
+        x_mu (:,1) double;
+        f_mu (:,1) double;
         k (:,1) double {mustBeInteger, mustBePositive};
     end
     
-    m = numel(x);
+    m = numel(x_mu);
     assert(k<m-1,"k < m-1 is required for this algorithm.")
     
-    assert(m==numel(f),"x and f must have the same number of elements.");
+    assert(m==numel(f_mu),"x and f must have the same number of elements.");
     
     %% Algorithm
     % This algorithm is 
@@ -48,15 +100,15 @@ function [resultsTable] = polyfitOrtho(x,f,k)
     % initialize
     p_iMinus1 = zeros(m,1); % p_(-1)
     p_i = ones(m,1); % p_0
-    deltaSquarediMinus1 = f'*f;
+    deltaSquarediMinus1 = f_mu'*f_mu;
     resultsTable = table(nan(k+1,1), nan(k+1,1), nan(k+1,1), nan(k+1,1),'VariableNames',["s","alpha","beta","variance"]);
-    resultsTable.beta(1) = 0;
+    resultsTable.beta(1) = 0; % beta_0
     resultsTable.Row = "" + (0:k)';
     wii = m;
     
     for i=0:k
-        omegai = f'*p_i;
-        resultsTable.s(i+1) = omegai/wii;
+        omegai = f_mu'*p_i;
+        resultsTable.s(i+1) = omegai/wii; % s_i
         deltaSquaredi = deltaSquarediMinus1 - resultsTable.s(i+1)^2*wii;
         resultsTable.variance(i+1) = deltaSquaredi./(m-i-1);
         % TODO. We could test here if the variance is low enough.
@@ -65,14 +117,14 @@ function [resultsTable] = polyfitOrtho(x,f,k)
             break
         end
         
-        resultsTable.alpha(i+2) = (x.*p_i)'*p_i/wii;
+        resultsTable.alpha(i+2) = (x_mu.*p_i)'*p_i/wii; % alpha_(i+1)
         
         % Prepare for next loop
-        p_iPlus1 = (x-resultsTable.alpha(i+2)).*p_i - resultsTable.beta(i+1)*p_iMinus1;
+        p_iPlus1 = (x_mu-resultsTable.alpha(i+2)).*p_i - resultsTable.beta(i+1)*p_iMinus1;
         p_iMinus1 = p_i;
         p_i = p_iPlus1;
         wiPlus1iPlus1 = p_iPlus1'*p_iPlus1;
-        resultsTable.beta(i+2) = wiPlus1iPlus1/wii;
+        resultsTable.beta(i+2) = wiPlus1iPlus1/wii; % beta_(i+1)
         wii = wiPlus1iPlus1;
         deltaSquarediMinus1 = deltaSquaredi;
         
