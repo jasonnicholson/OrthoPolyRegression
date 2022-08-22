c86c6ca098cd2a710ce53114c589063a1ff0c407
Jason H Nicholson <1058191+jasonnicholson@users.noreply.github.com>
2022-08-21 20:16:43 -0500

    Intermediate commit.

--- polyfitOrtho.m
+++ polyfitOrtho.m
@@ -1,7 +1,36 @@
-function [coeffTable] = orthoPolyFit(x,f,k)
-    %UNTITLED Summary of this function goes here
-    %   Detailed explanation goes here
+function [resultsTable] = polyfitOrtho(x,f,k)
+    %polyfitOrtho fits polynomial orthogonal
+    %   resultsTable = polyfitOrtho(x,f,k)
+    %
+    %% Inputs
+    % x - vector of x points for fit.
+    % f - vector of y points for fit. Must be the same size as x.
+    %
+    %% Outputs
+    % resultsTable - 
+    %% Description
+    %
+    %% Example
+    % Fit a 100th degree polynomial to the Runge function on 150 Chebyshev points. Compare to polyfit/Vandermonde
+    % matrices. The monomial basis, x^(0:n), is exponentially ill-conditioned as n increases. polyfit/Vandermonde cannot
+    % fit 100th degree polynomial.
+    %   runge = @(x) 1./(1+25*x.^2);
+    %   theta = linspace(pi,0,150);
+    %   x = cos(theta); % Chebyshev points
+    %   y = runge(x);
+    %   k = 100;
+    %   resultsTable = polyfitOrtho(x,y,k);
+    %
+    %
+    %% References
+    % The algorithm and nomenclature comes from Forsythe 1957.
+    % Forsythe, George E. "Generation and use of orthogonal polynomials for data-fitting with a digital computer."
+    % Journal of the Society for Industrial and Applied Mathematics 5.2 (1957): 74-88.
+    %
+    % See also polyvalOrtho, polyfit, polyval.
+    %
     
+    %% Input Checking
     arguments
         x (:,1) double;
         f (:,1) double;
@@ -9,40 +38,41 @@ function [coeffTable] = orthoPolyFit(x,f,k)
     end
     
     m = numel(x);
+    assert(k<m-1,"k < m-1 is required for this algorithm.")
     
+    assert(m==numel(f),"x and f must have the same number of elements.");
+    
+    %% Algorithm
+    % This algorithm is 
     
     % initialize
-    piMinus1 = zeros(m,1);
-    pi = ones(m,1);
-    wii = m;
-    beta = nan(k+1,1);
-    beta(1) = 0;
+    p_iMinus1 = zeros(m,1); % p_(-1)
+    p_i = ones(m,1); % p_0
     deltaSquarediMinus1 = f'*f;
-    alpha = nan(k+1,1);
-    s = nan(k+1,1);
-    
-    coeffTable = table(s,alpha,beta,variance);
-    coeffTable.Row = "" + (0:k)';
+    resultsTable = table(nan(k+1,1), nan(k+1,1), nan(k+1,1), nan(k+1,1),'VariableNames',["s","alpha","beta","variance"]);
+    resultsTable.beta(1) = 0;
+    resultsTable.Row = "" + (0:k)';
+    wii = m;
     
     for i=0:k
-        omegai = f'*pi;
-        coeffTable.s(i+1) = omegai/wii;
-         deltaSquaredi = deltaSquarediMinus1 - coeffTable.s(i+1)^2*wii;
-%         variancei = deltaSquaredi./(m-i-1);
+        omegai = f'*p_i;
+        resultsTable.s(i+1) = omegai/wii;
+        deltaSquaredi = deltaSquarediMinus1 - resultsTable.s(i+1)^2*wii;
+        resultsTable.variance(i+1) = deltaSquaredi./(m-i-1);
         % TODO. We could test here if the variance is low enough.
         
         if i>=k
-            continue
+            break
         end
-                
-        coeffTable.alpha(i+2) = (x.*pi)'*pi/wii;
+        
+        resultsTable.alpha(i+2) = (x.*p_i)'*p_i/wii;
         
         % Prepare for next loop
-        piPlus1 = (x-coeffTable.alpha(i+2)).*pi - coeffTable.beta(i+1)*piMinus1;
-        piMinus1 = pi;
-        pi = piPlus1;
-        wiPlus1iPlus1 = piPlus1'*piPlus1;
-        coeffTable.beta(i+2) = wiPlus1iPlus1/wii;
+        p_iPlus1 = (x-resultsTable.alpha(i+2)).*p_i - resultsTable.beta(i+1)*p_iMinus1;
+        p_iMinus1 = p_i;
+        p_i = p_iPlus1;
+        wiPlus1iPlus1 = p_iPlus1'*p_iPlus1;
+        resultsTable.beta(i+2) = wiPlus1iPlus1/wii;
         wii = wiPlus1iPlus1;
         deltaSquarediMinus1 = deltaSquaredi;
         
