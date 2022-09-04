f759d1927498f85de98e74acf34dbfdb65db9a8b
Jason H Nicholson <1058191+jasonnicholson@users.noreply.github.com>
2022-09-03 22:15:38 -0500

    Add a script to look at the time complexity of polyfitOrtho and polyvalOrtho. Added documentation of the polyfitOrtho.

--- Analysis/timeComplexityOfpolyvalOrtho.m
+++ Analysis/timeComplexityOfpolyvalOrtho.m
@@ -1,22 +1,23 @@
-% Warning this takes 30 minutes or so to run.
-% Takeaway: The computational complexity of polyfitOrtho is O(k*m), bilinear.
-
 clc; clear; close all
 
 runge = @(x) 1./(1+25*x.^2);
+theta = linspace(pi,0,1e4);
+x = cos(theta);
+y = runge(x);
+k = 500;
+coefficientsAndResults = polyfitOrtho(x,y,k);
 
 intitialTime = cputime;
-kk = 10:500-1;
-mm = 500:500:10000;
+kk = 10:10:500-1;
+mm = 10000:10000:100000;
 times = nan(numel(kk),numel(mm));
 for j = 1:numel(mm)
     m = mm(j);
-    theta = linspace(pi,0,m);
-    x = cos(theta);
-    y = runge(x);
+    x = linspace(-1,1,m);
     for i = 1:numel(kk)
         k = kk(i);
-        f = @() polyfitOrtho(x,y,k);
+        s = structfun(@(x) x(1:k),coefficientsAndResults,'UniformOutput',false);
+        f = @() polyvalOrtho(x,s);
         times(i,j) = timeit(f);
     end
 end
@@ -29,4 +30,4 @@ c = [ones(numel(kkMatrix),1) kkMatrix(:) mmMatrix(:) kkMatrix(:).*mmMatrix(:)]\t
 hold all
 fsurf(@(k,m) c(1)+c(2)*k+c(3)*m+c(4)*k.*m,[xlim ylim])
 
-% time complexity is O(k*m)
+% time complexity is O(k*m), linear in k, linear in m, bilinear when both change.
